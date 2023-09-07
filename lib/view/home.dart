53a0cd08c303b4a7aa76370f11f6011aa99de4de
Animesh <animeshdeas@gmail.com>
2023-09-07 09:31:08 +0530

    Added Search Bar Finally

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class MyHome extends StatefulWidget {
@@ -10,29 +12,45 @@ class MyHome extends StatefulWidget {
 class _MyWidgetState extends State<MyHome> {
   @override
   Widget build(BuildContext context) {
+    
+    var city_name =["Bankok","Delhi","Asia","London","USA","China"];
+
+    final _random = new Random();
+    var city = city_name[_random.nextInt(city_name.length)]; 
+
+     
     return  Scaffold(
         appBar: AppBar(
           title: Text("News-Ley"),
           centerTitle: true,
+          backgroundColor: const Color.fromARGB(255, 163, 214, 255),
         ),
+       
         body: Column(
             children: [
              Container(
                
-                padding: EdgeInsets.all(8.0),
+                padding: EdgeInsets.symmetric(horizontal:8.0),
                 margin: EdgeInsets.symmetric(horizontal:24,vertical: 20),
                 decoration: BoxDecoration(
-                  color: Colors.grey,
-                  borderRadius:BorderRadius.circular(10),
+             
+                   color: Colors.transparent,
+                  
                    ),
                 child: Row(
                  children: [
-                     Icon(Icons.search),
+                     GestureDetector(
+                       onTap: ()=>{},
+                       child:Container(child:Icon(Icons.search,color: Colors.lightBlue,),margin: EdgeInsets.fromLTRB(3, 0, 7, 0),),
+                     ),
                      Expanded(
                       child: TextField(
+                         controller: SearchController(),
+                         textInputAction: TextInputAction.search,
+
                           decoration: InputDecoration(
                             border: InputBorder.none,
-                            hintText: "Search Any Country for News"
+                            hintText: "Search $city for News"
                           ),
                       ),
                       )
