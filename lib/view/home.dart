2b210529162497e3fcf92f6db84905e2e1dd7af6
Animesh <animeshdeas@gmail.com>
2023-09-09 19:42:47 +0530

    Added Category trying to fetch news

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
 import 'dart:math';
-
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:my_app/model/model.dart';
+import 'package:my_app/model/newsModel.dart';
 import 'package:http/http.dart' as http;
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:my_app/view/category.dart';
+
 
 
 
@@ -23,25 +25,28 @@ class _MyWidgetState extends State<MyHome> {
     //var city = cityName[random.nextInt(cityName.length)];
   
     List<String> navbarItem = [
-      "TopNews",
-      "Health",
-      "Finance",
-      "Cricket",
-      "Devotion"
+      "Top News",
+      "Business",
+      "Entertainment",
+      "Sports",
+      "Technology",
+      "Health"
     ];
 
    
      bool isLoading = true;
       List<NewsQueryModel> newsModelList = <NewsQueryModel>[];
+      List<NewsQueryModel> newsCountry = <NewsQueryModel>[];
 
   @override
   void initState() {
     super.initState();
-    getNewsByQuery("bitcoin");
+    getNews();
+    getNewsByCountry();
   }
 
-  Future<void> getNewsByQuery(String query) async {
-    String url = "https://newsapi.org/v2/everything?q=$query&apiKey=43031493986f4ecabe0c218c63472343";
+  Future<void> getNews() async {
+    String url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=43031493986f4ecabe0c218c63472343";
     
     try {
       final response = await http.get(Uri.parse(url));
@@ -67,6 +72,36 @@ class _MyWidgetState extends State<MyHome> {
       debugPrint('Error: $e');
     }
   }
+  
+   Future<void> getNewsByCountry() async {
+    String url = "https://newsapi.org/v2/top-headlines?country=in&category=general&apiKey=43031493986f4ecabe0c218c63472343";
+    
+    try {
+      final response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        final jsonData = json.decode(response.body);
+        final articles = jsonData["articles"] as List<dynamic>;
+
+        setState(() {
+          newsCountry.clear(); // Clear the list before adding new data
+
+          for (var element in articles) {
+            final newsQueryModel = NewsQueryModel.fromJson(element);
+            newsCountry.add(newsQueryModel);
+          }
+        });
+      } else {
+        // Handle error here if the request is not successful
+        debugPrint('Request failed with status: ${response.statusCode}');
+      }
+    } catch (e) {
+      // Handle exceptions here
+      debugPrint('Error: $e');
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -113,7 +148,6 @@ class _MyWidgetState extends State<MyHome> {
 
             Container(
               height: 50,
-              
               child: ListView.builder(
                   shrinkWrap: true,
                   scrollDirection: Axis.horizontal,
@@ -121,7 +155,7 @@ class _MyWidgetState extends State<MyHome> {
                   itemBuilder: (context, index) {
                     return InkWell(
                       onTap: () => {
-                        debugPrint(navbarItem[index]),
+                        Navigator.push(context, MaterialPageRoute(builder:(context)=>Category(query: navbarItem[index]))),
                       },
                       child: Container(
                         padding: const EdgeInsets.symmetric(
@@ -149,16 +183,17 @@ class _MyWidgetState extends State<MyHome> {
             Container(  
              
              margin: const EdgeInsets.symmetric(vertical: 15),
-
-            child: CarouselSlider(
+             child: CarouselSlider(
               options: CarouselOptions(
                   height: 200.0,
                   autoPlay: true,
                   enableInfiniteScroll: false,
                   enlargeCenterPage: true),
-              items: items.map((i) {
+              items: newsCountry.map((instance) {
+                 
                 return Builder(
                   builder: (BuildContext context) {
+                    
                     return Container(
                     
                       child: Card(
@@ -168,18 +203,30 @@ class _MyWidgetState extends State<MyHome> {
                         child: Stack(
                           
                           children: [
-                             
-                             ClipRRect(
-                              borderRadius: BorderRadius.circular(20),
-                              child: Image.asset("assets/images/news.jpg",fit: BoxFit.fitHeight,height: double.infinity,),
-                             ),
+                           
+                           ClipRRect(
+                                   borderRadius: BorderRadius.circular(10),
+                                   child: CachedNetworkImage(
+                                     placeholder: (context, url) => Image.asset(
+                                       'assets/images/news.jpg', // Placeholder image asset path
+                                       fit: BoxFit.cover,
+                                     ),
+                                     imageUrl: instance.newsImg,
+                                     fit: BoxFit.cover,
+                                     
+                                     errorWidget: (context, url, error) => Image.asset(
+                                       'assets/images/news.jpg', // Error placeholder image asset path
+                                       fit: BoxFit.cover,
+                                     ),
+                                   ),
+                                 ),
                              Positioned(
                               left: 0,
                               right: 0,
                               bottom: 0,
                               child: Container(
                                    decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(20),
+                                    borderRadius: BorderRadius.circular(10),
                                     gradient: LinearGradient(
                                        begin: Alignment.topCenter,
                                        end: Alignment.bottomCenter,
@@ -191,7 +238,7 @@ class _MyWidgetState extends State<MyHome> {
                                    ),
                                   child: Container(
                                     padding: const  EdgeInsets.symmetric(vertical: 5,horizontal: 10),
-                                    child: const Text("Current News HeadLine",style:TextStyle(fontSize: 14,fontWeight: FontWeight.bold,color: Colors.white),), 
+                                    child:  Text(instance.newsHead,style:TextStyle(fontSize: 14,fontWeight: FontWeight.bold,color: Colors.white),), 
                              ))),
                           ],
                         ) 
@@ -244,13 +291,13 @@ class _MyWidgetState extends State<MyHome> {
                                    placeholder: 'assets/images/news.jpg', 
                                    image: newsModelList[index].newsImg,
                                    fit: BoxFit.cover, 
-                                  
+                                 
                                    imageErrorBuilder: (context, error, stackTrace) {
                                     
                                      return Image.asset(
                                        'assets/images/news.jpg', // Replace with your error placeholder image asset path
                                        fit: BoxFit.cover,
-                                      
+                                  
                                      );
                                    },
                                  )
